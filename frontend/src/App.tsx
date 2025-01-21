18b2e675e7f8f2b33e69beffe14a492dec9cc72b
backnback <garam8796@naver.com>
2025-01-21 11:54:45 +0900

    front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,10 @@ function App() {
   console.log(import.meta.env.VITE_CORE_FRONT_BASE_URL);
   console.log(import.meta.env.VITE_CORE_API_BASE_URL);
 
+  fetch(import.meta.env.VITE_CORE_API_BASE_URL + "/")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
   return (
     <>
       <h1>Hello World! ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ</h1>
